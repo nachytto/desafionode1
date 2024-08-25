396c2fe040e82f319417914685629b4f1d506ece
nachytto <nacho.duartemiranda@gmail.com>
2024-08-25 17:52:46 -0400

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+// Importa registrar y leer desde operaciones.js.
 const { registrar, leer } = require('./operaciones');
+
+
+// process.argv es un array que contiene todos los argumentos pasados al script.
 
 const [, , operacion, nombre, edad, tipoanimal, color, enfermedad] = process.argv;
 
 
+// Se convierte edad a un número usando parseInt para trabajar mejor
 if (operacion === "registrar") {
     registrar(nombre, parseInt(edad), tipoanimal, color, enfermedad);
-} else if (operacion === "leer") {
+}
+else if (operacion === "leer") {
     leer();
-} else {
+} 
+
+else {
     console.log('Operación no reconocida. Usa "registrar" o "leer".');
 }
 
 
-// ejecutar:
+//instrucciones
 // node index.js registrar Benito "9 años" perro blanco vomitos
 // node index.js leer
+
--- operaciones.js
+++ operaciones.js
@@ -1,11 +1,15 @@
-//nose
+// Importa el módulo fs de Node.js, que permite trabajar con el sistema de archivos.
 const fs = require('fs');
 
-function registrar(nombre,edad,tipoanimal,color,enfermedad){
-
-    const citas = JSON.parse(fs.readFileSync('citas.json', 'utf8'));
+// Define la función registrar
+function registrar(nombre, edad, tipoanimal, color, enfermedad) {
     
-    const nuevaCita ={
+    // Lee el contenido del archivo citas.json y lo convierte de una cadena JSON a un objeto JavaScript.
+    // utf8 se usa para asegurarse de que el archivo se lea como texto en ese formato.
+    const citas = JSON.parse(fs.readFileSync('citas.json', 'utf8'));
+
+    // objeto nuevaCita
+    const nuevaCita = {
         nombre,
         edad,
         tipoanimal,
@@ -13,15 +17,19 @@ function registrar(nombre,edad,tipoanimal,color,enfermedad){
         enfermedad
     };
 
+    // Agrega el nuevo objeto nuevaCita al array de citas.
     citas.push(nuevaCita);
 
+    // Escribe el array actualizado de citas en su respectivo archivo.
+    // JSON.stringify convierte el array a una cadena JSON.
     fs.writeFileSync('citas.json', JSON.stringify(citas, null, 2), 'utf8');
-    console.log('cita registrada con exito');
-    
+    console.log('Cita registrada con éxito.');
 }
-function leer(){
-    const citas =JSON.parse(fs.readFileSync('citas.json','utf8'));
-    console.log('citas registradas:');
+
+function leer() {
+    // Lee el contenido del archivo citas.json y lo convierte de una cadena JSON a un objeto JavaScript.
+    const citas = JSON.parse(fs.readFileSync('citas.json', 'utf8'));
+    console.log('Citas registradas:');
     console.log(citas);
 }
-module.exports ={registrar, leer};
+module.exports = { registrar, leer };
